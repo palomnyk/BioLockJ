26fb19bdcdb674412e1b84414c4789742e70d8a4
Aaron_Y <amyerke@uncc.edu>
2020-02-17 16:32:25 -0500

    change port as React runs on port 3000

--- web_app/lib/constants.js
+++ web_app/lib/constants.js
@@ -1,6 +1,10 @@
-export const BLJ_PROJ = process.env.BLJ_PROJ, //path to where pipelines are saved
-  BLJ = process.env.BLJ, //path to blj
-  BLJ_CONFIG = process.env.BLJ_CONFIG, //path to where configs are saved
-  HOST_BLJ = process.env.HOST_BLJ; //path to host BLJ
+module.exports = Object.freeze({
+  //path constants
+  BLJ_PROJ: process.env.BLJ_PROJ, //path to where pipelines are saved
+  BLJ: process.env.BLJ, //path to blj
+  BLJ_CONFIG: process.env.BLJ_CONFIG, //path to where configs are saved
+  HOST_BLJ: process.env.HOST_BLJ, //path to host BLJ
 
-export const port = normalizePort(process.env.PORT || '3000');
+  //port contants
+  PORT: '4000'
+});
