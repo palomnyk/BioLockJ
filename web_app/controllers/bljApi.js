707499ab9bd979f70da2cdddda7aa35d570159e7
Aaron_Y <amyerke@uncc.edu>
2020-02-19 11:45:12 -0500

    reorg of comments with minor fixes.

--- web_app/controllers/bljApi.js
+++ web_app/controllers/bljApi.js
@@ -32,58 +32,11 @@ Options shown in [ ] are optional for a given query.
         file path for a configuration file giving one or more property values
   --verbose
         flag indicating that all messages should go to standard err, including some that are typically disabled.
-
-query:
-
-  listModules [ --external-modules <dir> ]
-        Returns a list of classpaths to the classes that extend BioModule.
-
-  listApiModules [--external-modules <dir> ]
-        Like listModules but limit list to modules that implement the ApiModule interface.
-
-  listProps [ --module <module_path> ]
-        Returns a list of properties.
-        If no args, it returns the list of properties used by the BioLockJ backbone.
-        If a modules is given, then it returns a list of all properties used by
-        that module.
-
-  listAllProps [ --external-modules <dir> ]
-        Returns a list of all properties, include all backbone properties and all module properties.
-        Optionally supply the path to a directory containing additional modules to include their properties.
-
-  propType --property <property> [ --module <module_path> [ --external-modules <dir> ] ]
-        Returns the type expected for the property: String, list, integer, positive number, etc.
-        If a module is supplied, then the modules propType method is used.
-
-  describeProp --property <property> [ --module <module_path> [ --external-modules <dir> ] ]
-        Returns a description of the property.
-        If a module is supplied, then the modules getDescription method is used.
-
-  propValue --property <property> [ --config <file> ]
-        Returns the value for that property given that config file (optional) or 
-        no config file (ie the default value)
-
-  isValidProp --property <property> --value <value> [ --module <module_path>  [--external-modules <dir>] ]
-        T/F/NA. Returns true if the value (val) for the property (prop) is valid;
-        false if prop is a property but val is not a valid value,
-        and NA if prop is not a recognized property.
-        IF a module is supplied, then additionally call the validateProp(key, value)
-        for that module, or for EACH module if a comma-separated list is given.
-
-  propInfo
-        Returns a json formatted list of the general properties (listProps)
-        with the type, descrption and default for each property
-
-  moduleInfo [--external-modules <dir>]
-        Returns a json formatted list of all modules and for each module that 
-        implements the ApiModule interface, it lists the props used by the module,
-        and for each prop the type, descrption and default.
-
-  help  (or no args)
-        Print help menu.
 */
 
 exports.listModules = function (req, res, next) {
+  // listModules [ --external-modules <dir> ]
+  // Returns a list of classpaths to the classes that extend BioModule.
   try {
     let param = req.body.jsonParam != undefined ? ` --external-modules ${req.body.jsonParam}` : "";
     console.log(`get req params: ${param}`);
@@ -104,8 +57,9 @@ exports.listModules = function (req, res, next) {
 }
 
 exports.listApiModules = function (req, res, next) {
+  // listApiModules [--external-modules <dir> ]
+  // Like listModules but limit list to modules that implement the ApiModule interface.
   try {
-    console.log(`get req params: ${String(req.body.jsonParam)}`);
     let param = req.body.jsonParam != undefined ? ` --external-modules ${req.body.jsonParam}` : "";
     let bljApi = spawn(`biolockj-api listApiModules ${param}`, {shell: '/bin/bash'});
     bljApi.stdout.on('data', function (data) {
@@ -124,6 +78,11 @@ exports.listApiModules = function (req, res, next) {
 }
 
 exports.listProps = function (req, res, next) {
+  // listProps [ --module <module_path> ]
+  // Returns a list of properties.
+  // If no args, it returns the list of properties used by the BioLockJ backbone.
+  // If a modules is given, then it returns a list of all properties used by
+  // that module.
   try {
     let param = req.body.jsonParam != undefined ? ` --module ${req.body.jsonParam}` : "";
     let bljApi = spawn(`biolockj-api listProps ${param}`, {shell: '/bin/bash'});
@@ -143,6 +102,9 @@ exports.listProps = function (req, res, next) {
 }
 
 exports.listAllProps = function (req, res, next) {
+  /*listAllProps [ --external-modules <dir> ]
+        Returns a list of all properties, include all backbone properties and all module properties.
+        Optionally supply the path to a directory containing additional modules to include their properties.*/
   try {
     console.log(`get req params: ${String(req.body.jsonParam)}`);
     let param = req.body.jsonParam != undefined ? ` --external-modules ${req.body.jsonParam}` : "";
@@ -163,6 +125,9 @@ exports.listAllProps = function (req, res, next) {
 }
 
 exports.propType = function (req, res, next) {
+  /*propType --property <property> [ --module <module_path> [ --external-modules <dir> ] ]
+        Returns the type expected for the property: String, list, integer, positive number, etc.
+        If a module is supplied, then the modules propType method is used.*/
   try {
     console.log(`get req params: ${String(req.body.prop)}`);
     let prop = req.body.prop != undefined ? ` --property ${req.body.prop}` : "";
@@ -184,6 +149,9 @@ exports.propType = function (req, res, next) {
   }
 }
 exports.describeProp = function (req, res, next) {
+  /*  describeProp --property <property> [ --module <module_path> [ --external-modules <dir> ] ]
+        Returns a description of the property.
+        If a module is supplied, then the modules getDescription method is used.*/
   try {
     let prop = req.body.prop != undefined ? ` --property ${req.body.prop}` : "";
     let mod = req.body.mod != undefined ? ` --module ${req.body.mod}` : "";
@@ -205,6 +173,9 @@ exports.describeProp = function (req, res, next) {
 }
 
 exports.propValue = function (req, res, next) {
+  /*propValue --property <property> [ --config <file> ]
+        Returns the value for that property given that config file (optional) or 
+        no config file (ie the default value)*/
   try {
     let prop = req.body.prop != undefined ? ` --property ${req.body.prop}` : "";
     let config = req.body.config != undefined ? ` --config ${req.body.config}` : "";
@@ -225,6 +196,13 @@ exports.propValue = function (req, res, next) {
 }
 
 exports.isValidProp = function (req, res, next) {
+  /* 
+    isValidProp --property <property> --value <value> [ --module <module_path>  [--external-modules <dir>] ]
+        T/F/NA. Returns true if the value (val) for the property (prop) is valid;
+        false if prop is a property but val is not a valid value,
+        and NA if prop is not a recognized property.
+        IF a module is supplied, then additionally call the validateProp(key, value)
+        for that module, or for EACH module if a comma-separated list is given.*/
   try {
     let prop = req.body.prop != undefined ? ` --property ${req.body.prop}` : "";
     let val = req.body.val != undefined ? ` --value ${req.body.val}` : "";
@@ -247,6 +225,11 @@ exports.isValidProp = function (req, res, next) {
 }
 
 exports.propInfo = function (req, res, next) {
+  /*
+    propInfo
+        Returns a json formatted list of the general properties (listProps)
+        with the type, descrption and default for each property
+        */
   try {
     let bljApi = spawn(`biolockj-api propInfo`, {shell: '/bin/bash'});
     bljApi.stdout.on('data', function (data) {
@@ -265,6 +248,11 @@ exports.propInfo = function (req, res, next) {
 }
 
 exports.moduleInfo = function (req, res, next) {
+  /* 
+    moduleInfo [--external-modules <dir>]
+        Returns a json formatted list of all modules and for each module that 
+        implements the ApiModule interface, it lists the props used by the module,
+        and for each prop the type, descrption and default.*/
   try {
     let extPath = req.body.extPath != undefined ? ` --external-modules ${req.body.extPath}` : "";
     let bljApi = spawn(`biolockj-api moduleInfo ${extPath}`, {shell: '/bin/bash'});
