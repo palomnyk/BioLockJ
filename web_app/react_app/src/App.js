039307338db2ed4a41be930d6d24a376a4e3a33b
Aaron_Y <amyerke@uncc.edu>
2020-03-17 00:32:09 -0400

    App imports configGenerator

--- web_app/react_app/src/App.js
+++ web_app/react_app/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import logo from './images/BioLockJ_NoBG_green.svg';
 import './App.css';
-import './tests/apiTests.js'
+// import './tests/apiTests.js'
+// import ModulesUnselected from './components/bljModule/modulesUnselected.js'
+import ConfigGenerator from './components/configGenerator.js'
 
 
 class App extends React.Component{
@@ -47,50 +49,58 @@ class App extends React.Component{
     ]
 
   */
-  // this.state = {
-  //   pipelines: [ 
-  //     {
-  //       pipeline: "16sRdpPipeline1",
-  //       project: "Wastewater project",
-  //       date_created: "2018-8-1",
-  //       date_modified: "2018-8-1",
-  //       date_run:"2018-8-1",
-  //       pipeline_location: "/pipelines/16sRdpPipeline1-2018Aug1",
-  //       config_location: "/pipelines/16sRdpPipeline1-2018Aug1/16sRdpPipeline1.properties",
-  //       successful_run: true,
-  //       minimized: true,
-  //     },
-  //     {
-  //       pipeline: "ITsRdpPipeline1",
-  //       project: "Wastewater project",
-  //       date_created: "2018-8-30",
-  //       date_modified: "2018-8-30",
-  //       date_run:"2018-8-30",
-  //       pipeline_location: "/pipelines/ITsRdpPipeline1-2018Apr30",
-  //       config_location: "/pipelines/ITsRdpPipeline1-2018Apr30/16sRdpPipeline1.properties",
-  //       successful_run: true,
-  //       minimized: true,
-  //     },
-  //     {
-  //       pipeline: "ITsRdpPipeline2",
-  //       project: "Wastewater project",
-  //       date_created: "2018-9-30",
-  //       date_modified: "2018-9-30",
-  //       date_run:null,
-  //       pipeline_location: null,
-  //       config_location: "/pipelines/ITsRdpPipeline1-2018Apr30/16sRdpPipeline1.properties",
-  //       successful_run: false,
-  //       minimized: true,
-  //   },
-  //   ]
+  state = {
+    pipelines: [ 
+      {
+        pipeline: "16sRdpPipeline1",
+        project: "Wastewater project",
+        date_created: "2018-8-1",
+        date_modified: "2018-8-1",
+        date_run:"2018-8-1",
+        pipeline_location: "/pipelines/16sRdpPipeline1-2018Aug1",
+        config_location: "/pipelines/16sRdpPipeline1-2018Aug1/16sRdpPipeline1.properties",
+        successful_run: true,
+        minimized: true,
+      },
+      {
+        pipeline: "ITsRdpPipeline1",
+        project: "Wastewater project",
+        date_created: "2018-8-30",
+        date_modified: "2018-8-30",
+        date_run:"2018-8-30",
+        pipeline_location: "/pipelines/ITsRdpPipeline1-2018Apr30",
+        config_location: "/pipelines/ITsRdpPipeline1-2018Apr30/16sRdpPipeline1.properties",
+        successful_run: true,
+        minimized: true,
+      },
+      {
+        pipeline: "ITsRdpPipeline2",
+        project: "Wastewater project",
+        date_created: "2018-9-30",
+        date_modified: "2018-9-30",
+        date_run:null,
+        pipeline_location: null,
+        config_location: "/pipelines/ITsRdpPipeline1-2018Apr30/16sRdpPipeline1.properties",
+        successful_run: false,
+        minimized: true,
+    },
+    ]
+  };
+  // renderModules(){
+  //   return (
+  //     <modulesUnselected/>
+  //   );
+    
   // }
+  // console.table(state);
   render(){
     return (
       <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
-        <header className="App-header">
-          App
-        </header>
+        {/* <header className="App-header">
+          App 
+        </header> */}
+          <ConfigGenerator/>
       </div>
     );
   }
